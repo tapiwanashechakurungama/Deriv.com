15088a47dc7fade83059ac181db714b5d43731e8
Tapiwanashe Chakurungama <chakurungamatapiwanashe@gmail.com>
2024-07-25 09:23:03 +0200

    updating

--- main.js
+++ main.js
@@ -1,19 +1,16 @@
 const tradeShows = document.querySelector(".trade");
-const body = document.body
+const body = document.body;
 const ham = document.querySelector(".hamburger");
 
+tradeShows.addEventListener("mouseover", () => {
+  body.classList.add("show-trades");
+});
+tradeShows.addEventListener("mouseout", () => {
+  body.classList.remove("show-trades");
+});
 
-
-tradeShows.addEventListener("mouseover",()=>{
-  body.classList.add("show-trades")
-})
-tradeShows.addEventListener("mouseout",()=>{
-  body.classList.remove("show-trades")
-})
-
-ham.addEventListener("click",(event)=>{
-  body.classList.toggle("active")
-  body.classList.toggle("cross")
-
-})
-
+ham.addEventListener("click", (event) => {
+  body.classList.toggle("active");
+  body.classList.toggle("cross");
+});
+window.addEventListener("scroll", () => {});
