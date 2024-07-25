be35f983ca53f05b5f2120ce2ba5184792983284
Tapiwanashe Chakurungama <chakurungamatapiwanashe@gmail.com>
2024-07-25 12:33:47 +0200

    updating

--- main.js
+++ main.js
@@ -14,3 +14,22 @@ ham.addEventListener("click", (event) => {
   body.classList.toggle("cross");
 });
 window.addEventListener("scroll", () => {});
+
+
+const slider = document.querySelector(".slider");
+const slides = slider.querySelectorAll("li");
+
+// Store the total number of images
+const slideCount = slides.length;
+let activeSlide = 0;
+
+// To change the images dynamically every
+// 5 Secs, use SetInterval() method
+setInterval(() => {
+  slides[activeSlide].classList.remove("active");
+  activeSlide++;
+  if (activeSlide === slideCount) {
+    activeSlide = 0;
+  }
+  slides[activeSlide].classList.add("active");
+}, 2000);
